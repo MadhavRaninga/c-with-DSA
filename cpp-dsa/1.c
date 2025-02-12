cc2b6113fb92909f0633ca588d96fae47e06158f
MadhavRaninga <madhavraninga@gmail.com>
2025-02-12 14:15:08 +0530

    madhav

---
+++ cpp-dsa/1.c
@@ -0,0 +1,20 @@
+// ans = 1.
+//  programming is process that we can handle by our code and we can some tasks by using programming language like c , c++ , DSA  is called programming.
+
+
+//ans = 2.
+//A translator is a converting a code written by us it covert in code which understand a computer is called a  translator . 
+
+//ans = 3.
+// 1 = Compiler
+// 2 = Interpreter
+// 3 = Assembler
+
+//ans = 4. 
+      //  Compiler             |             Interpreter    
+     //                        |
+    // 1. Translate the all    |    1. Translate the code in 
+    // code of source          |        line-by-line
+    //                         |
+    // 2. it is faster in      |    2. it is slower in 
+    // translate a code .      |       translate a code .
